86e9d8dded1f155f4d93b88debe74bc7e0c09030
PaulsenEU <pawelsz18@gmail.com>
2022-05-25 20:11:51 +0200

    Initial commit

---
+++ src/test/java/com/javabiz/auctionsite/AuctionSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabiz.auctionsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
