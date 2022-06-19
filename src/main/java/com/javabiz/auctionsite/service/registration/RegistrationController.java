e45a67eb80dbffcd3b764ebf09dd4d5b70aa7bf4
Paweł Awramiuk <awramiukofficial@gmail.com>
2022-06-19 10:02:23 +0200

    Wywalone zbędne pliki, działa przekierowanie na registration i login, ale nie działa sama rejestracja

--- src/main/java/com/javabiz/auctionsite/service/registration/RegistrationController.java
+++ src/main/java/com/javabiz/auctionsite/service/registration/RegistrationController.java
@@ -3,24 +3,49 @@ package com.javabiz.auctionsite.service.registration;
 import com.javabiz.auctionsite.service.model.UserModel;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@RestController
-@RequestMapping(path = "registration")
-@AllArgsConstructor
+@Controller
 public class RegistrationController {
 
     @Autowired
     private RegistrationService registrationService;
 
-    @PostMapping
-    public UserModel register(@Valid @RequestBody RegistrationRequest request) {
-        return registrationService.register(request);
+    @GetMapping("/registration")
+    public String processRegister(Model model) {
+        UserModel user = new UserModel();
+        model.addAttribute("user", user);
+        return "login/registration";
+    }
+
+    @PostMapping("/registration")
+    public String register(@Valid @ModelAttribute("user") UserModel user, @RequestBody RegistrationRequest request) {
+        registrationService.register(request);
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value="/logout", method=RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
 
